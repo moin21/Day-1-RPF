3ec93e3bf335e5c2189de139e2231d841e23dca4
moin21 <mamoinuddin@gmail.com>
2022-02-05 15:50:00 +0530

    added: daily wage logic

--- EmployeeWage/Uc1_EmployeeAttendance.java
+++ EmployeeWage/Uc1_EmployeeAttendance.java
@@ -3,15 +3,18 @@ package EmployeeWage;
 public class Uc1_EmployeeAttendance {
 
         public static void main(String[] args) {
-            int workHrs;
+            int workHrs = 8;
+            int perHrWage = 20;
+            int dayWage = workHrs * perHrWage;
             int empAttendance =(int)(Math.random()*10)%2;
 
             if(empAttendance == 1) {
-                workHrs=8;
                 System.out.println("Employee is present Today an will work for " + workHrs + " hours");
+                System.out.println("Today's wage is " + dayWage + " Rupees");
+
             }
             else {
-                System.out.println("Employee is absent today");
+                System.out.println("Employee is absent today, and total wage is zero");
             }
         }
 }
